c8e1ddd9381999f254501ef111186a9d61d9c0a9
unknown <argya.programmer@gmail.com>
2020-10-17 12:26:15 +0700

    Initial Commit

---
+++ src/com/tugasPBO/latihan2PBO.java
@@ -0,0 +1,23 @@
+//latihan 2 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan deklarasi variabel
+package com.tugasPBO;
+
+public class latihan2PBO {
+    public static void main(String[] args){
+        // deklarasi variabel dan isinya
+        int nilaiINT = 78;
+        final double phii = 3.14;
+        boolean nilaiLogika = false;
+        char nilaiKarakter = 'D';
+
+        // cetak
+        System.out.println("Isi variabel nilaiInt : " + nilaiINT);
+        System.out.println("Isi variabel phi : " + phii);
+        System.out.println("Isi variabel nilaiLogika : " + nilaiLogika);
+        System.out.println("Isi variabel nilaiKarakter : " + nilaiKarakter);
+
+    }
+}
